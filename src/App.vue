17d8841ee839704d58206ab4efefd0dd0776d5c0
Alina-KI <kraynova.alina.2002@mail.ru>
2022-12-19 14:12:26 +0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@
       <InventoryInfo/>
       <Inventory/>
     </div>
-
+    <div class="footer">
+      <div class="line"/>
+      <button class="close">
+        <img class="cross" src="src/components/icons/CarbonClose.svg" alt="">
+      </button>
+    </div>
   </div>
 </template>
 
@@ -29,13 +34,46 @@ export default {
   gap: 24px;
   cursor: url("src/components/icons/default-cursor.svg"), auto;
 }
-.inventory{
+
+.inventory {
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   gap: 24px;
   width: 100%;
-  height: 100%;
+}
+
+.footer{
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 785px;
+  background: #262626;
+  border: 1px solid #4D4D4D;
+  border-radius: 12px;
+}
+.line{
+  width: 100%;
+  height: 36px;
+  background: linear-gradient(90deg, #3C3C3C 0%, #444444 51.04%, #333333 100%);
+  border-radius: 12px;
+  margin: 18px 68px 18px 18px;
+}
+.close{
+  background: transparent;
+  border: none;
+  outline: none;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+}
+
+.cross{
+  width: 24px;
+  height: 24px;
 }
 </style>
