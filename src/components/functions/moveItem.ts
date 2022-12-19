af5b99da3d21dd8070252418644f15152836db4b
Alina-KI <kraynova.alina.2002@mail.ru>
2022-12-19 13:22:57 +0300

    fixed moveItem

--- src/components/functions/moveItem.ts
+++ src/components/functions/moveItem.ts
@@ -2,12 +2,23 @@ import type {Item} from "@/types/item";
 
 export const moveItem = (arr: Item[], item: Item, old_index: number, new_index: number) => {
     const newItem = arr.find(i => i.id === new_index)!
-    return arr.map((arrItem) => {
-            if (arrItem.id === old_index)
-                return newItem
-            if (arrItem.id === new_index)
-                return item
-            return arrItem
-        }
-    );
+    if (newItem.image === '')
+        return arr.map((arrItem, index) => {
+                if (arrItem.id === old_index)
+                    return {
+                        ...newItem,
+                        id: index
+                    }
+                if (arrItem.id === new_index)
+                    return {
+                        ...item,
+                        id: index
+                    }
+                return {
+                    ...arrItem,
+                    id: index
+                }
+            }
+        );
+    return arr
 };
